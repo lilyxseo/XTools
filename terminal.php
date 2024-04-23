dc531168e90790e22c89f31658cd310c7b6309e5
Abi Baydarus <x@bydrz.com>
2024-04-23 20:52:18 +0700

    delete terminal

--- terminal.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['command'])) {
-    $command = 'sudo ' . $_POST['command']; // Tambahkan 'sudo' di sini
-    $output = shell_exec($command);
-    echo $output;
-} else {
-    echo "Invalid request";
-}
-?>
