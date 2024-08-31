da26f487af06737ef30c5c85a459071c7cc4429d
Abi Baydarus <x@bydrz.com>
2024-08-31 23:18:14 +0700

    update dashboard

--- dashboard.php
+++ dashboard.php
@@ -350,7 +350,7 @@ if (isset($_POST["tambahData"])) {
                                                         <label for="nominal3" class="form-label">Nominal:</label>
                                                         <div class="input-group">
                                                             <span class="input-group-text">Rp.</span>
-                                                            <input type="text" class="form-control" id="nominal3" name="nominal" placeholder="500.000" required pattern="[0-9]+(?:\.[0-9]{1,2})?*" required inputmode='numeric'>
+                                                            <input type="text" class="form-control" id="nominal2" name="nominal" placeholder="500.000" required pattern="[0-9]+(?:\.[0-9]{1,2})?*" required inputmode='numeric'>
                                                         </div>
                                                     </div>
                                                     <div class="mb-3">
